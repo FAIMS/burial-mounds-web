8c684cb4e9a381cb6cbd035450e61c646dd3d553
Terry Bui <terry.bui96@gmail.com>
2018-03-21 19:00:27 +1100

    photo gallery and google maps working for trap mound 1000

---
+++ assets/js/index.js
@@ -0,0 +1,51 @@
+ $('.slider-for').slick({
+   slidesToShow: 1,
+   slidesToScroll: 3,
+   arrows: false,
+   fade: true,
+
+   asNavFor: '.slider-nav'
+ });
+ $('.slider-nav').slick({
+   slidesToShow: 3,
+   slidesToScroll: 3,
+   arrows: false,
+   asNavFor: '.slider-for',
+
+   focusOnSelect: true,
+   variableWidth: false,
+   responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+ });
+
+ $('a[data-slide]').click(function(e) {
+   e.preventDefault();
+   var slideno = $(this).data('slide');
+   $('.slider-nav').slick('slickGoTo', slideno - 1);
+ });
