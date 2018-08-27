6fbc2d7c727bca27868441c6d4c22c989494dacf
Terry-Bui <terry.bui96@gmail.com>
2018-08-27 19:54:12 +1000

    pylint for make_site, csv_merge rename python module to snake case

---
+++ _import/csv_merge.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import argparse
+import pandas as pd
+
+PARSER = argparse.ArgumentParser(
+    description='A program that takes two csv and merge the two csv with the \
+     column name passed as the key. It returns a new csv with supplied name')
+PARSER.add_argument('-k', '--key', help='Key to merge on')
+PARSER.add_argument('-o', '--output', help='Name of output file')
+PARSER.add_argument('-f', '--first', help='Name of first file')
+PARSER.add_argument('-s', '--second', help='Name of second file')
+
+# To take a list
+# PARSER.add_argument('-n', '--names-list', nargs='+', default=[])
+
+ARGS = vars(PARSER.parse_args())
+
+KEY = ARGS['key']
+OUTPUT = ARGS['output']
+FIRST = ARGS['first']
+SECOND = ARGS['second']
+
+FIRST_CSV = pd.read_csv(FIRST)
+SECOND_CSV = pd.read_csv(SECOND)
+
+SECOND_CSV = SECOND_CSV.dropna(axis=1)
+
+print(FIRST_CSV.head())
+print(SECOND_CSV.head())
+
+# Sanitize KEY, replace space with '_' and make it key lowercase
+#KEY = KEY.replace(' ','_').lower()
+
+MERGED = FIRST_CSV.merge(SECOND_CSV, on=KEY)
+print(MERGED.head())
+MERGED.to_csv(OUTPUT, index=False)
--- _import/make_site.py
+++ _import/make_site.py
@@ -2,17 +2,8 @@
 
 import csv
 import os
-import sys
-from pprint import pprint
-import ruamel.yaml
-from ruamel.yaml import YAML
-import shutil
-from pathlib import Path
 import datetime
-import re
-import fnmatch
-import glob
-
+from ruamel.yaml import YAML
 import dumpYaml as DUMP
 
 SHEET = "MergeCSV.csv"
@@ -20,30 +11,30 @@ TEMPLATE = "template.yaml"
 DEST = "../_posts"
 
 # Name of the collection of the Records Pages
-collectionname = "TRAP Mounds"
+COLLECTION_NAME = "TRAP Mounds"
 
 # The list that will be used to hold dictonaries where each dictionary
 # correspond to a row in the csv
-objects = []  # PLEASE DO NOT MODIFY
+OBJECTS = []  # PLEASE DO NOT MODIFY
 # The list that will be used to hold the column names of the csv
-headers = []  # PLEASE DO NOT MODIFY
+CSV_HEADINGS = []  # PLEASE DO NOT MODIFY
 
 # Name of the column for the uuid
-uuid = 'TRAP ID'
+UUID_COLUMN_NAME = 'TRAP ID'
 
 
 # A generic title for each Record Page which will be concatenated with the
 # uuid of each Record
-title = "TRAP Mound - "
+TITLE = "TRAP Mound - "
 
-# Creates a list of dictionary where each dict correspond to a row in the csv, each key correspond to the column names and store that
-# into the list objects
+# Creates a list of dictionary where each dict correspond to a row in the csv,
+# each key correspond to the column names and store that into the list objects
 # PLEASE DO NOT MODIFY THE NEXT 5 LINES OF CODE
 with open(SHEET, newline='') as csvfile:
-    sheetreader = csv.DictReader(csvfile)
-    headers = sheetreader.fieldnames
-    for row in sheetreader:
-        objects.append(row)
+    SHEET_READER = csv.DictReader(csvfile)
+    CSV_HEADINGS = SHEET_READER.fieldnames
+    for row in SHEET_READER:
+        OBJECTS.append(row)
 
 # Make the directory where the all the Record Pages will be created in
 os.makedirs('../_posts/', exist_ok=True)
@@ -52,22 +43,24 @@ print("Generating Record Pages")
 
 # Loop through objects list, each item in objects is a record
 # create a corresponding page for that record with its front matter variables
-for obj in objects:
-    # id stores the uuid of the record, the strip() method removes leading or trailing characters
-    # refer to https://docs.python.org/3/library/stdtypes.html#str.strip for
+for obj in OBJECTS:
+    # record_id stores the uuid of the record, the strip() method removes leading or
+    # trailing characters refer to
+    # https://docs.python.org/3/library/stdtypes.html#str.strip for
     # more information
-    id = obj[uuid].strip()  # PLEASE DO NOT MODIFY
+    record_id = obj[UUID_COLUMN_NAME].strip()  # PLEASE DO NOT MODIFY
 
     # target is the path of where the file for the Record will be made
     # this is done by joining the destination with the filename
     target = os.path.join(DEST,
                           "{0}-{1}.md".format(datetime.date.today(),
-                                              'TRAP' + id))  # PLEASE DO NOT MODIFY
+                                              'TRAP' + record_id))  # PLEASE DO NOT MODIFY
     yaml = YAML()  # PLEASE DO NOT MODIFY
     with open(TEMPLATE) as templatefile:  # PLEASE DO NOT
         objyaml = yaml.load(templatefile.read())
         if 'subheadline' in objyaml:
-            # Change the assigned value of objyaml['subheadline'] to your needs, for example, changing
+            # Change the assigned value of objyaml['subheadline']
+            # to your needs,for example, changing
             # objyaml['subheadline'] = "Collection: TRAP Mounds"  to
             # objyaml['subheadline'] = "Collection: Another type of collection"
             # will change the subheadline front matter variables of a Record to
@@ -75,7 +68,7 @@ for obj in objects:
             objyaml['subheadline'] = "Collection: TRAP Mounds"
 
         # Assign the id of the record into the uuid front matter variable
-        objyaml['uuid'] = id
+        objyaml['uuid'] = record_id
 
         if 'Tag' in obj:
             objyaml['tags'] = obj['Tags'].split(' | ')
@@ -85,15 +78,15 @@ for obj in objects:
         categories = []
         # Change the following if and else if statements according to your
         # needs
-        if int(id) in range(1000, 2000):
+        if int(record_id) in range(1000, 2000):
             categories.append("1000-1999")
-        elif int(id) in range(2000, 3000):
+        elif int(record_id) in range(2000, 3000):
             categories.append("2000-2999")
-        elif int(id) in range(3000, 4000):
+        elif int(record_id) in range(3000, 4000):
             categories.append("3000-3999")
-        elif int(id) in range(4000, 5000):
+        elif int(record_id) in range(4000, 5000):
             categories.append("4000-4999")
-        elif int(id) in range(5000, 6000):
+        elif int(record_id) in range(5000, 6000):
             categories.append("5000-5999")
 
         objyaml['categories'] = categories
@@ -104,20 +97,20 @@ for obj in objects:
 
         # Add key:value pairs to the front matter, where each key correspond to a column
         # and the value is the value of the column for that row
-        for k in headers:
+        for k in CSV_HEADINGS:
             k_with_no_space = k.replace(" ", "_").lower()
             # Exclude adding the key:value pair for the column that is the uuid
             # that is because, we have it in the front matter variable uuid
-            if k != uuid:
+            if k != UUID_COLUMN_NAME:
                 objyaml[k_with_no_space] = obj[k]
 
         # Assign value to the front matter variable title
-        objyaml['title'] = title + id
+        objyaml['title'] = TITLE + record_id
 
         # If there are no images then remove the images front matter for that Record
         # so that the Record photo gallery will display default image instead
         objyaml['images'] = []
-        if len(objyaml['images']) == 0:
+        if not objyaml['images']:
             objyaml.pop('images', None)
 
         # This will write the value of objyaml which contains the front matter variable
