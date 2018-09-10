8c5cdbbd94c98ce433c3a3f5433257d867f99b26
Terry-Bui <terry.bui96@gmail.com>
2018-09-10 20:24:56 +1000

    refactor JS file in assets/custom_js directory extract variables use strict for slick-settings.js

--- assets/custom_js/home_page_map.js
+++ assets/custom_js/home_page_map.js
@@ -5,7 +5,8 @@ exclude_from_lunr: true
 function initHomePageMap() {
  var centreLatLng = {lat: 42.6169, lng: 25.2848 };
 
- var map = new google.maps.Map(document.getElementById('home_page_map'),{
+ var homePageMapHTMLElement = document.getElementById('home_page_map');
+ var map = new google.maps.Map(homePageMapHTMLElement, {
    center: centreLatLng,
    zoom: 11
  });
@@ -13,11 +14,12 @@ function initHomePageMap() {
   [
   {% for post in site.posts %}
   {"title":"{{ post.title }}","lat":"{{ post.latitude}}","lng":"{{ post.longitude }}"}
-  {% if forloop.last %}{% else %},{% endif %}
+  {%- if forloop.last -%}{%- else -%},{%- endif -%}
   {% endfor %}
   ]
   {% endcapture %}
   var markers = {{markers | strip_newlines}};
+
   for(var i = 0; i < markers.length; i++){
     var latLng = new google.maps.LatLng(markers[i].lat, markers[i].lng);
     var marker = new google.maps.Marker({
@@ -27,15 +29,18 @@ function initHomePageMap() {
     marker.setMap(map);
   }
 
+  var RED = '#FF0000'; // Deep RED Color in HEX
+  var RADIUS_AREA = 15 * 1000; // In metres so 15km
+
   var cityCircle = new google.maps.Circle({
-      strokeColor: '#FF0000',
+      strokeColor: RED,
       strokeOpacity: 0.8,
       strokeWeight: 2,
-      fillColor: '#FF0000',
+      fillColor: RED,
       fillOpacity: 0.35,
       map: map,
       center: centreLatLng,
-      radius: 15*1000
+      radius: RADIUS_AREA
     });
 
 }
